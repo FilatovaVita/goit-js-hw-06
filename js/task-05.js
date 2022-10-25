c3f0b83ede381daab8974117227ea4d917254f02
Vita Filatova <filatova.vitta@gmail.com>
2022-10-25 11:21:44 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,8 @@ const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
   textOutput.textContent = event.currentTarget.value;
+
+  if (textOutput.textContent === "") {
+    textOutput.textContent = "Anonymous";
+  }
 });
